47dbde3b77d607dacb97592b8dd3ec1238772826
erlendthune <erlend.thune@gmail.com>
2024-07-01 22:51:54 +0200

    Fix collapse bug and pricing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
   ],
   scripts: [
     '/garmin/sql-wasm.js',
-    '/garmin/garminscript.js?v=9'
+    '/garmin/garminscript.js?v=10'
   ],
   plugins: [
     './garmin-plugin-script',
